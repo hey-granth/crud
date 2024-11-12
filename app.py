6584d4c0ee254d21a9d6e131eb71767adeeec1ea
Granth Agarwal <heygranth@gmail.com>
2024-11-12 19:48:34 +0530

    minor changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '1234mpgd'
 app.config['MYSQL_DB'] = 'crud_app'
 
 mysql = MySQL(app)
