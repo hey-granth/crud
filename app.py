ac65d159826775140e6baa20c475337375fc8a28
hey-granth <heygranth@gmail.com>
2024-11-13 12:04:13 +0530

    formatted code

--- app.py
+++ app.py
@@ -3,58 +3,70 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234mpgd'
-app.config['MYSQL_DB'] = 'crud_app'
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "1234mpgd"
+app.config["MYSQL_DB"] = "crud_app"
 
 mysql = MySQL(app)
-@app.route('/')
+
+
+@app.route("/")
 def Index():  # put application's code here
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM students")
     data = cur.fetchall()
     cur.close()
 
-    return render_template('index.html', students=data)
+    return render_template("index.html", students=data)
 
-@app.route('/insert', methods = ['POST'])
+
+@app.route("/insert", methods=["POST"])
 def insert():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        cur.execute(
+            "INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)",
+            (name, email, phone),
+        )
         mysql.connection.commit()
-        return redirect(url_for('Index'))
+        return redirect(url_for("Index"))
 
-@app.route('/delete/<string:id_data>', methods = ['GET'])
+
+@app.route("/delete/<string:id_data>", methods=["GET"])
 def delete(id_data):
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM students WHERE id=%s", (id_data,))
     mysql.connection.commit()
-    flash('Record has been deleted successfully')
-    return redirect(url_for('Index'))
+    flash("Record has been deleted successfully")
+    return redirect(url_for("Index"))
 
-@app.route('/update', methods = ['POST', 'GET'])
+
+@app.route("/update", methods=["POST", "GET"])
 def update():
-    if request.method == 'POST':
-        id_data = request.form['id']
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
+    if request.method == "POST":
+        id_data = request.form["id"]
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
         cur = mysql.connection.cursor()
-        cur.execute("""
+        cur.execute(
+            """
             UPDATE students
             SET name=%s, email=%s, phone=%s
             WHERE id=%s
-        """, (name, email, phone, id_data))
-        flash('Data Updated Successfully')
+        """,
+            (name, email, phone, id_data),
+        )
+        flash("Data Updated Successfully")
         mysql.connection.commit()
-        return redirect(url_for('Index'))
+        return redirect(url_for("Index"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
 
 
